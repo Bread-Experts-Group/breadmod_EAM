fbd42c97140ccc7be5542cd5333e428b5afcfd94
ATPStorages <tcgytsc@gmail.com>
2026-03-02 07:08:50 -0600

    BSLening

--- src/main/kotlin/org/bread_experts_group/breadmod/BMUtil.kt
+++ src/main/kotlin/org/bread_experts_group/breadmod/BMUtil.kt
@@ -1,12 +1,16 @@
 package org.bread_experts_group.breadmod
 
+import org.bread_experts_group.eam.minecraft.feature.MinecraftMod.Companion.modID
 import org.bread_experts_group.eam.minecraft.version_impl.v1x21x1.net.minecraft.core.Vec3i
 import org.bread_experts_group.eam.minecraft.version_impl.v1x21x1.net.minecraft.resources.ResourceLocation
 
 object BMUtil {
 	fun modLocation(vararg path: String): ResourceLocation =
 		path.toMutableList().let {
-			ResourceLocation.fromNamespaceAndPath("breadmod", it.joinToString("/"))
+			ResourceLocation.fromNamespaceAndPath(
+				BreadMod::class.java.modID,
+				it.joinToString("/")
+			)
 		}
 
 	operator fun Vec3i.component1(): Int = this.getX()
